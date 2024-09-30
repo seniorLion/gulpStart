59a11171f71796dba15e5e6d513a205d05e13baf
Igori Galanchuk <prieomnaeakomissiea@gmail.com>
2024-09-30 16:48:14 +0300

    final

--- gulpfile.js
+++ gulpfile.js
@@ -9,18 +9,57 @@ const avif = require('gulp-avif');
 const webp = require('gulp-webp');
 const imagemin = require('gulp-imagemin');
 const newer = require('gulp-newer');
+const fonter = require('gulp-fonter');
+const ttf2woff2 = require('gulp-ttf2woff2');
+const svgSprite = require('gulp-svg-sprite');
+const include = require('gulp-include')
+
+function pages() {
+  return src('app/pages/*.html')
+    .pipe(include({
+      includePaths: 'app/components'
+    }))
+    .pipe(dest('app'))
+    .pipe(browserSync.stream())
+}
+
+function fonts() {
+  return src('app/fonts/src/*.*')
+    .pipe(fonter({
+      formats: ['woff', 'ttf']
+    }))
+    .pipe(src('app/fonts/*.ttf'))
+    .pipe(ttf2woff2())
+    .pipe(dest('app/fonts'))
+}
 
 function images() {
-  return src('app/images/src/*.*', '!app/images/src/*.svg')
+  return src(['app/images/src/*.*', '!app/images/src/*.svg'])
+  .pipe(newer('app/images/dist'))
   .pipe(avif({ quality: 50}))
 
   .pipe(src('app/images/src/*.*'))
+  .pipe(newer('app/images/dist'))
   .pipe(webp())
 
   .pipe(src('app/images/src/*.*'))
+  .pipe(newer('app/images/dist'))
   .pipe(imagemin())
 
   .pipe(dest('app/images/dist'))
+}
+
+function sprite() {
+  return src('app/images/dist/*.svg')
+    .pipe(svgSprite({
+      mode: {
+        stack: {
+          sprite: '../sprite.svg',
+          example: true
+        }
+      }
+    }))
+    .pipe(dest('app/images/dist'))
 }
 
 function styles() {
@@ -47,7 +86,9 @@ function watching() {
     }
 });
   watch(['app/css/style.scss'], styles)
+  watch(['app/images/src'], images)
   watch(['app/js/main.js'], scripts)
+  watch(['app/components/*', 'app/pages/*'], pages)
   watch(['app/**/*.html']).on('change', browserSync.reload)
 }
 
@@ -59,6 +100,10 @@ function cleanDist() {
 function building() {
   return src([
     'app/css/style.min.css',
+    'app/images/dist/*.*',
+    '!app/images/dist/*.svg',
+    'app/images/dist/sprite.svg',
+    'app/fonts/*.*',
     'app/js/main.min.js',
     'app/**/*.html'
   ], {base: 'app'})
@@ -67,7 +112,11 @@ function building() {
 
 exports.styles = styles;
 exports.images = images;
+exports.fonts = fonts;
+exports.pages = pages;
+exports.building = building;
+exports.sprite = sprite;
 exports.scripts = scripts;
 exports.watching = watching;
 exports.build = series(cleanDist, building);
-exports.default = parallel(styles, images, scripts, watching);
+exports.default = parallel(styles, images, scripts, pages, watching);
